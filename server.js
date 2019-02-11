2ef6afb422bda5fff9e12a879b6244c591359c61
Steven Deferme <sdeferme@quant.be>
2019-02-11 19:38:42 +0100

    Add project page

--- server.js
+++ server.js
@@ -50,13 +50,19 @@ hbs.registerHelper('upperCase', (text) => {
 app.get('/', (req, res) => {
     res.render('home.hbs', {
         pageTitle: 'Home page',
-        welcomeMessage: 'Welcome'
+        welcomeMessage: 'Welcome page text would go here'
     });
 });
 
 app.get('/about', (req, res) => {
     res.render('about.hbs', {
         pageTitle: 'About page',
+    });
+});
+
+app.get('/projects', (req, res) => {
+    res.render('projects.hbs', {
+        pageTitle: 'Projects page',
     });
 });
 
